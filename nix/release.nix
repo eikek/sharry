01fce706c68f5b0af26c1ebff5fcd698b7f21eed
Eike Kettner <eike.kettner@posteo.de>
2020-03-16 22:01:57 +0100

    Update release.nix

--- nix/release.nix
+++ nix/release.nix
@@ -1,5 +1,12 @@
 rec {
   cfg = {
+    v1_1_0 = rec {
+      version = "1.1.0";
+      src = {
+        url = "https://github.com/eikek/sharry/releases/download/release%2F${version}/sharry-restserver-${version}.zip";
+        sha256 = "0c50v444npfrdlsw3qp4cni4pi4pfyzaijhh9bfm2xn5pz7ib9sm";
+      };
+    };
     v1_0_0 = rec {
       version = "1.0.0";
       src = {
@@ -9,7 +16,7 @@ rec {
     };
   };
   pkg = v: import ./pkg.nix v;
-  currentPkg = pkg cfg.v1_0_0;
+  currentPkg = pkg cfg.v1_1_0;
   module = ./module.nix;
   modules = [ module
             ];
