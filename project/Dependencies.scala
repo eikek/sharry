86d7aaaa7fe1263839a60c8b69bf3a44c4f28211
Scala Steward <me@scala-steward.org>
2020-04-07 00:59:34 +0200

    Update minitest, minitest-laws to 2.8.2

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -17,7 +17,7 @@ object Dependencies {
   val Log4sVersion            = "1.8.2"
   val LogbackVersion          = "1.2.3"
   val MariaDbVersion          = "2.6.0"
-  val MiniTestVersion         = "2.8.1"
+  val MiniTestVersion         = "2.8.2"
   val PostgresVersion         = "42.2.12"
   val PureConfigVersion       = "0.12.3"
   val SemanticUIVersion       = "2.4.1"
