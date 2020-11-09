d20247da476360abc3dcb2e7d19f2405eae2fc17
Scala Steward <me@scala-steward.org>
2020-11-09 15:36:18 +0100

    Update flyway-core to 7.2.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -9,7 +9,7 @@ object Dependencies {
   val ClipboardJsVersion      = "2.0.6"
   val DoobieVersion           = "0.9.2"
   val EmilVersion             = "0.6.3"
-  val FlywayVersion           = "7.1.1"
+  val FlywayVersion           = "7.2.0"
   val FomanticUIVersion       = "2.8.7-3"
   val Fs2Version              = "2.4.4"
   val H2Version               = "1.4.200"
