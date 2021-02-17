51201de69f9c702966d8b2c6b284a4cf2095cedb
Scala Steward <me@scala-steward.org>
2021-02-17 02:34:12 +0100

    Update emil-common, emil-javamail to 0.7.2

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -8,7 +8,7 @@ object Dependencies {
   val CirceVersion            = "0.13.0"
   val ClipboardJsVersion      = "2.0.6"
   val DoobieVersion           = "0.10.0"
-  val EmilVersion             = "0.7.1"
+  val EmilVersion             = "0.7.2"
   val FlywayVersion           = "7.5.3"
   val FomanticUIVersion       = "2.8.7-3"
   val Fs2Version              = "2.5.0"
