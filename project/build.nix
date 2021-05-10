9e0dd6ac2ab762b67acfb4e81c937c005c67dcea
Eike Kettner <eike.kettner@posteo.de>
2021-05-10 10:20:15 +0200

    Simplify build.nix

--- project/build.nix
+++ project/build.nix
@@ -1,14 +1,10 @@
 with import <nixpkgs> { };
-let
-  initScript = writeScript "sharry-build-init" ''
-     export LD_LIBRARY_PATH=
-     sbt "$@"
-  '';
-in
-buildFHSUserEnv {
+mkShell {
   name = "sharry-sbt";
-  targetPkgs = pkgs: with pkgs; [
-    netcat jdk8 wget which zsh dpkg sbt git ncurses mc jekyll fakeroot elmPackages.elm
+  buildInputs = [
+    dpkg sbt git jekyll fakeroot elmPackages.elm
   ];
-  runScript = initScript;
+  shellHook = ''
+
+  '';
 }
