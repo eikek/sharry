e5e3fe987252d5f6b549a0de0e6989ba99e48ac1
Eike Kettner <eike.kettner@posteo.de>
2021-05-21 21:50:29 +0200

    Show number and size of all shares

--- modules/webapp/src/main/elm/Page/Upload/View.elm
+++ modules/webapp/src/main/elm/Page/Upload/View.elm
@@ -8,6 +8,7 @@ import Messages.UploadPage exposing (Texts)
 import Page exposing (Page(..))
 import Page.Upload.Data exposing (Model, Msg(..))
 import Styles as S
+import Util.Size
 
 
 view : Texts -> Model -> Html Msg
@@ -20,9 +21,22 @@ view texts model =
 
 viewList : Texts -> Model -> List (Html Msg)
 viewList texts model =
+    let
+        allSize =
+            List.map .size model.searchResult
+                |> List.sum
+                |> toFloat
+                |> Util.Size.bytesReadable Util.Size.B
+
+        count =
+            List.length model.searchResult
+    in
     [ h1 [ class S.header1 ]
         [ i [ class "fa fa-share-alt mr-2" ] []
         , text texts.yourShares
+        , span [ class "text-sm opacity-75 ml-1" ]
+            [ text ("#" ++ String.fromInt count ++ "/" ++ allSize)
+            ]
         ]
     , searchArea texts model
     , Html.map ShareTableMsg
