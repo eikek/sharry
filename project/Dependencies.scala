10deff6dfb495701a8a609d998a8876917deeea2
Scala Steward <me@scala-steward.org>
2021-07-26 18:37:13 +0200

    Update logback-classic to 1.2.5

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -16,7 +16,7 @@ object Dependencies {
   val JQueryVersion           = "3.5.1"
   val KindProjectorVersion    = "0.10.3"
   val Log4sVersion            = "1.10.0"
-  val LogbackVersion          = "1.2.4"
+  val LogbackVersion          = "1.2.5"
   val MariaDbVersion          = "2.7.3"
   val MUnitVersion            = "0.7.27"
   val OrganizeImportsVersion  = "0.5.0"
