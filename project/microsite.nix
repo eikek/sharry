25f4782f0f5ccf533686f0c4246d0418f17eb2d8
eikek <eike.kettner@posteo.de>
2021-08-12 18:47:26 +0200

    Build microsite via github actions

--- project/microsite.nix
+++ project/microsite.nix
@@ -1,8 +1,9 @@
 let
   nixpkgs = builtins.fetchTarball {
-    url = "https://github.com/NixOS/nixpkgs/archive/refs/tags/20.09.tar.gz";
-  };
-  pkgs = import nixpkgs { };
+    #url = "channel:nixos-21.05";
+    url = "https://github.com/NixOS/nixpkgs/archive/2d6ab6c6b92f7aaf8bc53baba9754b9bfdce56f2.tar.gz";
+    #sha256 = "0l975q132x08qvw73qj391dl6qk9a661my8njcg5sl5rcmna3bmj";
+  };  pkgs = import nixpkgs { };
   run-jekyll = pkgs.writeScript "run-jekyll" ''
     jekyll serve -s modules/microsite/target/site --baseurl /sharry
   '';
