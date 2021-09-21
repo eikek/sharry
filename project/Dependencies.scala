09b16a285fd36ae75248bf30f0c4eb6da1363c60
Scala Steward <me@scala-steward.org>
2021-09-21 16:32:05 +0200

    Update http4s-blaze-client, ... to 0.23.4

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -12,7 +12,7 @@ object Dependencies {
   val FlywayVersion           = "7.15.0"
   val Fs2Version              = "3.1.2"
   val H2Version               = "1.4.200"
-  val Http4sVersion           = "0.23.3"
+  val Http4sVersion           = "0.23.4"
   val JQueryVersion           = "3.5.1"
   val KindProjectorVersion    = "0.10.3"
   val Log4sVersion            = "1.10.0"
