ca55f3b5450f7084244451e9815a85694472ab45
Scala Steward <me@scala-steward.org>
2021-09-23 16:23:34 +0200

    Update postgresql to 42.2.24

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -20,7 +20,7 @@ object Dependencies {
   val MariaDbVersion          = "2.7.4"
   val MUnitVersion            = "0.7.29"
   val OrganizeImportsVersion  = "0.5.0"
-  val PostgresVersion         = "42.2.23"
+  val PostgresVersion         = "42.2.24"
   val PureConfigVersion       = "0.16.0"
   val SwaggerVersion          = "3.52.1"
   val TikaVersion             = "2.1.0"
