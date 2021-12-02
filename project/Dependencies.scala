640b717505b793f8d742631537f7bdec3badfbb7
Scala Steward <me@scala-steward.org>
2021-12-02 01:08:50 +0100

    Update yamusca-core to 0.8.2

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -25,7 +25,7 @@ object Dependencies {
   val SwaggerVersion = "4.1.2"
   val TikaVersion = "2.1.0"
   val TusClientVersion = "1.8.0-1"
-  val YamuscaVersion = "0.8.1"
+  val YamuscaVersion = "0.8.2"
 
   val fs2 = Seq(
     "co.fs2" %% "fs2-core" % Fs2Version
