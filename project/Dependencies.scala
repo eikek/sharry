2b9f054872a2491dde4bdef5a886ea53dc781484
Scala Steward <me@scala-steward.org>
2022-03-16 00:34:22 +0100

    Update flyway-core to 8.5.4

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -9,7 +9,7 @@ object Dependencies {
   val ClipboardJsVersion = "2.0.6"
   val DoobieVersion = "1.0.0-RC2"
   val EmilVersion = "0.11.2"
-  val FlywayVersion = "8.5.3"
+  val FlywayVersion = "8.5.4"
   val Fs2Version = "3.2.5"
   val H2Version = "2.1.210"
   val Http4sVersion = "0.23.10"
