638a912961c3191d724c8cae35a63bec35fc29fc
Scala Steward <me@scala-steward.org>
2022-05-24 21:26:37 +0200

    Update swagger-ui to 4.11.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -22,7 +22,7 @@ object Dependencies {
   val PureConfigVersion = "0.17.1"
   val ScribeVersion = "3.8.0"
   val SourcecodeVersion = "0.2.8"
-  val SwaggerVersion = "4.10.3"
+  val SwaggerVersion = "4.11.1"
   val TikaVersion = "2.4.0"
   val TusClientVersion = "1.8.0-1"
   val YamuscaVersion = "0.9.0"
