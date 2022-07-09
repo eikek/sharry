4af47b879491999157412f8819555e3182cc9e89
eikek-scala-steward <eikek-scala-steward@users.noreply.github.comb>
2022-07-09 06:14:58 +0000

    Update fs2-core, fs2-io to 3.2.10

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -10,7 +10,7 @@ object Dependencies {
   val DoobieVersion = "1.0.0-RC2"
   val EmilVersion = "0.12.0"
   val FlywayVersion = "8.5.13"
-  val Fs2Version = "3.2.9"
+  val Fs2Version = "3.2.10"
   val H2Version = "2.1.214"
   val Http4sVersion = "0.23.13"
   val JQueryVersion = "3.5.1"
