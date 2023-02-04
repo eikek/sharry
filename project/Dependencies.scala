87b1ca062cb2afb0b5e2fa1312090696428aae61
eikek-scala-steward <eikek-scala-steward@users.noreply.github.comb>
2023-02-04 06:17:15 +0000

    Update tika-core to 2.7.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -23,7 +23,7 @@ object Dependencies {
   val ScribeVersion = "3.9.0"
   val SourcecodeVersion = "0.2.8"
   val SwaggerVersion = "4.15.5"
-  val TikaVersion = "2.6.0"
+  val TikaVersion = "2.7.0"
   val TusClientVersion = "1.8.0-1"
   val YamuscaVersion = "0.10.0"
 
