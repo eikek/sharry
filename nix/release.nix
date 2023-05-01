fb55fef1690198750478dd4f39d837fc0c4a0735
eikek <eikek@posteo.de>
2023-05-01 19:16:23 +0200

    Update nix setup

--- nix/release.nix
+++ nix/release.nix
@@ -1,10 +1,10 @@
 rec {
   cfg = {
-    v1_12_0 = rec {
-      version = "1.12.0";
+    v1_12_1 = rec {
+      version = "1.12.1";
       src = {
         url = "https://github.com/eikek/sharry/releases/download/v${version}/sharry-restserver-${version}.zip";
-        sha256 = "sha256-c2IzEMlgUdld7H7efSNPOl47KPg4AJ/tKHywMPx9S1g=";
+        sha256 = "sha256-XiP5s7wJWcTA4zkuDTHWp3+UVvloz3MFrpgboLIz0Ww=";
       };
     };
     v1_11_0 = rec {
@@ -58,7 +58,7 @@ rec {
     };
   };
   pkg = v: import ./pkg.nix v;
-  currentPkg = pkg cfg.v1_12_0;
+  currentPkg = pkg cfg.v1_12_1;
   module = ./module.nix;
   modules = [ module
             ];
