55a11c7c9ffce73b346da340ab90126fb491152f
Scala Steward <scala_steward@virtuslab.com>
2023-06-14 14:54:30 +0000

    Update swagger-ui to 5.0.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -23,7 +23,7 @@ object Dependencies {
   val PureConfigVersion = "0.17.4"
   val ScribeVersion = "3.11.5"
   val SourcecodeVersion = "0.3.0"
-  val SwaggerVersion = "4.19.1"
+  val SwaggerVersion = "5.0.0"
   val TikaVersion = "2.8.0"
   val TusClientVersion = "1.8.0-1"
   val YamuscaVersion = "0.10.0"
