4db3af4285ec1d8d0ce371a3d0fc7ac7ab62c45e
Scala Steward <scala_steward@virtuslab.com>
2023-06-22 14:11:17 +0000

    Update doobie-core, doobie-hikari to 1.0.0-RC4

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -7,7 +7,7 @@ object Dependencies {
   val BinnyVersion = "0.9.1"
   val CirceVersion = "0.14.5"
   val ClipboardJsVersion = "2.0.11"
-  val DoobieVersion = "1.0.0-RC2"
+  val DoobieVersion = "1.0.0-RC4"
   val EmilVersion = "0.13.0"
   val FlywayVersion = "9.20.0"
   val Fs2Version = "3.6.1"
