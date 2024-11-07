dac92cc0fbe48794cc61b7ced0203bfad80bfab9
Scala Steward <scala_steward@virtuslab.com>
2024-11-07 15:18:31 +0000

    Update flyway-core, ... to 10.21.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -10,7 +10,7 @@ object Dependencies {
   val ClipboardJsVersion = "2.0.11"
   val DoobieVersion = "1.0.0-RC6"
   val EmilVersion = "0.17.0"
-  val FlywayVersion = "10.20.1"
+  val FlywayVersion = "10.21.0"
   val Fs2Version = "3.11.0"
   val H2Version = "2.3.232"
   val Http4sVersion = "0.23.29"
