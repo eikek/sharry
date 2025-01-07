e6b5eb7a81af862b024dcc02ba0e8eb07de2bc25
Scala Steward <scala_steward@virtuslab.com>
2025-01-07 12:05:42 +0000

    Update munit to 1.0.4

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -17,7 +17,7 @@ object Dependencies {
   val JQueryVersion = "3.5.1"
   val KindProjectorVersion = "0.10.3"
   val MariaDbVersion = "3.5.1"
-  val MUnitVersion = "1.0.0"
+  val MUnitVersion = "1.0.4"
   val MUnitCatsEffectVersion = "2.0.0"
   val PostgresVersion = "42.7.4"
   val ScribeVersion = "3.15.3"
